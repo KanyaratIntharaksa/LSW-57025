0103f04847006929adbc338b68f065dce7cd2700
KanyaratIntharaksa <98764662+KanyaratIntharaksa@users.noreply.github.com>
2022-03-01 13:03:40 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ไอจี</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Min Yoongi</td>
+        <td>Suga</td>
+        <td><img src="4de70280-33be-11eb-90fa-996a3a236e84_original.jpg" width="130px"></td>
+        <td><a href="https://www.instagram.com/agustd/">instagram</a>
+       </tr>  
+    </table>
+  </body> 
+</html>
