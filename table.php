1e253bb80667fa6be891e05a84851a6d086ee704
KanyaratIntharaksa <98764662+KanyaratIntharaksa@users.noreply.github.com>
2022-03-05 20:33:17 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,37 +15,37 @@
         <td>1</td>
         <td>Min Yoongi</td>
         <td>Suga</td>
-        <td><img src="https://www.google.com/search?q=%E0%B8%8A%E0%B8%B9%E0%B8%81%E0%B9%89%E0%B8%B2+bts&tbm=isch&ved=2ahUKEwjoxfnBgK_2AhWPk9gFHSg1ArgQ2-cCegQIABAA&oq=%E0%B8%8A%E0%B8%B9%E0%B8%81%E0%B9%89%E0%B8%B2+bts&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCwgAEIAEELEDEIMBMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BQgAELEDUMIFWLk4YKo-aABwAHgAgAGjAYgBgQaSAQMwLjWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=2VsjYqj1CY-n4t4PqOqIwAs&bih=789&biw=1600#imgrc=OclPI9qpu68W7M"130px"></td>
+        <td><img src="https://s.isanook.com/jo/0/ud/485/2425871/suga.jpg"130px"></td>
         <td><a href="https://www.instagram.com/agustd/">instagram</a>
        </tr>
            <td>2</td>
         <td>Park Jimin</td>
         <td>Jimin</td>
-        <td><img src="" width="130px"></td>
+        <td><img src="https://i.pinimg.com/originals/fd/72/fb/fd72fb1984a708c0ebb9085d3c6e5187.jpg" width="130px"></td>
         <td><a href="https://www.instagram.com/accounts/login/?next=/j.m/">instagram</a></td>
       <tr>
         <td>3</td>
         <td>Kim Taehyung</td>
         <td>V</td>
-        <td><img src="" width="130px"></td>
+        <td><img src="https://ssl.pstatic.net/mimgnews/image/108/2021/10/15/0002996947_001_20211015073613648.jpg?%20type=w540" width="130px"></td>
         <td><a href="https://www.instagram.com/accounts/login/?next=/thv/">instagram</a></td>
       <tr>
         <td>4</td>
         <td>ยอร์ช ยงศิลป์</td>
         <td>ยอร์ช</td>
-        <td><img src="" width="130px"></td>
+        <td><img src="https://s.isanook.com/ca/0/ui/278/1394137/41977754_1155410461280302_244432773852963287_n_1548837537.jpg" width="130px"></td>
         <td><a href="https://www.instagram.com/accounts/login/?next=/yorch_yongsin/">instagram</a></td>
       <tr>
         <td>5</td>
         <td>park chaeyoung</td>
         <td>Rose</td>
-        <td><img src="" width="130px"></td>
+        <td><img src="https://entertain.teenee.com/kpop/img0/1609568.jpg" width="130px"></td>
         <td><a href="https://www.instagram.com/accounts/login/?next=/roses_are_rosie/">instagram</a></td>
       <tr>
         <td>6</td>
         <td>lee ji-eun</td>
         <td>iu</td>
-        <td><img src="" width="130px"></td>
+        <td><img src="https://www.thaiticketmajor.com/variety/img_news/title/original1/0410/12410/title_ttmnews_12410-20191129182420.jpg" width="130px"></td>
         <td><a href="https://www.instagram.com/accounts/login/?next=/dlwlrma/">instagram</a></td>
     </table>
   </body> 
