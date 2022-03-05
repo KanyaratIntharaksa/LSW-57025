f0a96f2818ec31ec3ed9680b5093841276cdf3a5
KanyaratIntharaksa <98764662+KanyaratIntharaksa@users.noreply.github.com>
2022-03-05 19:35:26 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,7 +17,36 @@
         <td>Suga</td>
         <td><img src=""130px"></td>
         <td><a href="https://www.instagram.com/agustd/">instagram</a>
-       </tr>  
+       </tr>
+           <td>2</td>
+        <td>Park Jimin</td>
+        <td>Jimin</td>
+        <td><img src="" width="130px"></td>
+        <td><a href="https://www.instagram.com/accounts/login/?next=/j.m/">instagram</a></td>
+      <tr>
+        <td>3</td>
+        <td>Kim Taehyung</td>
+        <td>V</td>
+        <td><img src="" width="130px"></td>
+        <td><a href="https://www.instagram.com/accounts/login/?next=/thv/">instagram</a></td>
+      <tr>
+        <td>4</td>
+        <td>ยอร์ช ยงศิลป์</td>
+        <td>ยอร์ช</td>
+        <td><img src="" width="130px"></td>
+        <td><a href="https://www.instagram.com/accounts/login/?next=/yorch_yongsin/">instagram</a></td>
+      <tr>
+        <td>5</td>
+        <td>park chaeyoung</td>
+        <td>Rose</td>
+        <td><img src="" width="130px"></td>
+        <td><a href="https://www.instagram.com/accounts/login/?next=/roses_are_rosie/">instagram</a></td>
+      <tr>
+        <td>6</td>
+        <td>lee ji-eun</td>
+        <td>iu</td>
+        <td><img src="" width="130px"></td>
+        <td><a href="https://www.instagram.com/accounts/login/?next=/dlwlrma/">instagram</a></td>
     </table>
   </body> 
 </html>
