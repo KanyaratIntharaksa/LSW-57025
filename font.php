976e6c32cb1cb7ab5f8ed1601f5f1cb1faec6532
KanyaratIntharaksa <98764662+KanyaratIntharaksa@users.noreply.github.com>
2022-03-05 19:58:56 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url()
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url()
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวกัลยารัตน์ อินทรักษา ม.4/13 เลขที่ 16 เลขประจำตัว 57025</h1>
+  <h1 id="font2">นางสาวกัลยารัตน์ อินทรักษา ม.4/13 เลขที่ 16 เลขประจำตัว 57025</h1>
+</body>
+</html>
